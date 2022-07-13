892b52aa63a3fa8a5ce97e06303aaf641ddfba46
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-07-13 19:38:56 +0900

    fix error

--- java-algorithm/src/main/java/com/example/demo/JavaAlgorithmApplication.java
+++ java-algorithm/src/main/java/com/example/demo/JavaAlgorithmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaAlgorithmsApplication {
+public class JavaAlgorithmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaAlgorithmsApplication.class, args);
+		SpringApplication.run(JavaAlgorithmApplication.class, args);
 	}
 
 }
