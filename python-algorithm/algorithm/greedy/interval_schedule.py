a842da81d66477a4c7854084499d1932fb23a754
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-08-04 06:21:40 +0900

    update syntax

--- python-algorithm/algorithm/greedy/interval_schedule.py
+++ python-algorithm/algorithm/greedy/interval_schedule.py
@@ -1,13 +1,19 @@
-def interval_scheduling_talk(talks: list[dict]) -> list[dict]:
-    talks.sort(key=lambda t: t['start'])
-    talks.sort(key=lambda t: t['end'])
-    scheduled_talks = []
+def interval_scheduling_lecture(lectures: list[dict]) -> list[dict]:
+    """
+    Greedy algorithm to schedule lectures.
+    :param lectures: list of lectures
+    :return: list of scheduled lectures
+    """
 
-    for index, talk in enumerate(talks):
-        if not scheduled_talks:
-            scheduled_talks.append(talk)
+    lectures.sort(key=lambda t: t['start'])
+    lectures.sort(key=lambda t: t['end'])
+    scheduled_lectures = []
+
+    for index, lecture in enumerate(lectures):
+        if not scheduled_lectures:
+            scheduled_lectures.append(lecture)
         else:
-            if talk['start'] >= scheduled_talks[-1]['end']:
-                scheduled_talks.append(talk)
+            if lecture['start'] >= scheduled_lectures[-1]['end']:
+                scheduled_lectures.append(lecture)
 
-    return scheduled_talks
+    return scheduled_lectures
--- python-algorithm/test/greedy/test_interval_schedule.py
+++ python-algorithm/test/greedy/test_interval_schedule.py
@@ -1,17 +1,17 @@
 import time
 
-from algorithm.greedy.interval_schedule import interval_scheduling_talk
+from algorithm.greedy.interval_schedule import interval_scheduling_lecture
 
 
-def test_schedule_talk():
-    talk1 = {'title': 'talk1',
-             'start': time.strptime('2022-07-25 08:00:00', '%Y-%m-%d %H:%M:%S'),
-             'end': time.strptime('2022-07-25 12:00:00', '%Y-%m-%d %H:%M:%S')}
-    talk2 = {'title': 'talk2',
-             'start': time.strptime('2022-07-25 09:00:00', '%Y-%m-%d %H:%M:%S'),
-             'end': time.strptime('2022-07-25 10:00:00', '%Y-%m-%d %H:%M:%S')}
-    talk3 = {'title': 'talk3',
-             'start': time.strptime('2022-07-25 11:00:00', '%Y-%m-%d %H:%M:%S'),
-             'end': time.strptime('2022-07-25 12:00:00', '%Y-%m-%d %H:%M:%S')}
+def test_schedule_lecture():
+    lecture1 = {'title': 'lecture1',
+                'start': time.strptime('2022-07-25 08:00:00', '%Y-%m-%d %H:%M:%S'),
+                'end': time.strptime('2022-07-25 12:00:00', '%Y-%m-%d %H:%M:%S')}
+    lecture2 = {'title': 'lecture2',
+                'start': time.strptime('2022-07-25 09:00:00', '%Y-%m-%d %H:%M:%S'),
+                'end': time.strptime('2022-07-25 10:00:00', '%Y-%m-%d %H:%M:%S')}
+    lecture3 = {'title': 'lecture3',
+                'start': time.strptime('2022-07-25 11:00:00', '%Y-%m-%d %H:%M:%S'),
+                'end': time.strptime('2022-07-25 12:00:00', '%Y-%m-%d %H:%M:%S')}
 
-    assert interval_scheduling_talk([talk1, talk2, talk3]) == [talk2, talk3]
+    assert interval_scheduling_lecture([lecture1, lecture2, lecture3]) == [lecture2, lecture3]
