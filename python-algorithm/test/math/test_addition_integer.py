67b982c5afd743a876deb1e2675b658d7e0f6565
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-08-19 20:12:26 +0900

    fix error

--- python-algorithm/test/math/test_addition_integer.py
+++ python-algorithm/test/math/test_addition_integer.py
@@ -1,4 +1,4 @@
-from algorithm.math.binary_operation import addition_binary_number, multiplication_binary_number
+from algorithm.math.binary_operation import addition_binary_number
 
 
 def test_addition_of_binary_number():
