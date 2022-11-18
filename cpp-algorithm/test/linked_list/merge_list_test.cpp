2a742cc8c08f3d0696ac7ff7eaef7ab1192be719
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-11-19 03:11:39 +0900

    update test cases for the merge list problem

--- cpp-algorithm/test/linked_list/merge_list_test.cpp
+++ cpp-algorithm/test/linked_list/merge_list_test.cpp
@@ -2,7 +2,7 @@
 
 #include <gtest/gtest.h>
 
-GTEST_TEST(MergeListTest, MergeTwoSortedLinkedList)
+GTEST_TEST(MergeListTest, MergeTwoSortedLinkedList1)
 {
     const auto node_a3 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{4, nullptr});
     const auto node_a2 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{2, node_a3});
@@ -16,7 +16,25 @@ GTEST_TEST(MergeListTest, MergeTwoSortedLinkedList)
     const auto expected = std::vector<int>{1, 1, 2, 3, 4, 4};
     for (const auto& i : expected)
     {
-        EXPECT_EQ(i, result->data);
+        ASSERT_EQ(i, result->data);
+        result = result->next;
+    }
+}
+
+GTEST_TEST(MergeListTest, MergeTwoSortedLinkedList2)
+{
+    const auto node_a3 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{7, nullptr});
+    const auto node_a2 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{5, node_a3});
+    const auto node_a1 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{2, node_a2});
+
+    const auto node_b2 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{11, nullptr});
+    const auto node_b1 = std::make_shared<LinkedList::Node<int>>(LinkedList::Node<int>{3, node_b2});
+
+    auto result = MergeList::MergeTwoSortedLinkedList(node_a1, node_b1);
+    const auto expected = std::vector<int>{2, 3, 5, 7, 11};
+    for (const auto& i : expected)
+    {
+        ASSERT_EQ(i, result->data);
         result = result->next;
     }
 }
