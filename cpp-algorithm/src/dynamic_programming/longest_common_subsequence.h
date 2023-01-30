22c7bbdf99b68d7e8b956e71093bbbf2cd0b2ad6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-01-30 14:51:58 +0900

    update comments

--- cpp-algorithm/src/dynamic_programming/longest_common_subsequence.h
+++ cpp-algorithm/src/dynamic_programming/longest_common_subsequence.h
@@ -7,32 +7,26 @@
 
 namespace CommonSubsequence
 {
-    /// <summary>
-    /// Compute the length of an LCS of two sequences
-    /// </summary>
-    /// <param name="seq1">string sequence 1</param>
-    /// <param name="seq2">string sequence 2</param>
-    /// <returns>length and direction matrices</returns>
+    /// @brief Compute the length of an LCS of two sequences
+    /// @param seq1 string sequence 1
+    /// @param seq2 string sequence 2
+    /// @return length and direction matrices
     auto LongestCommonSubsequenceLength(const std::string& seq1, const std::string& seq2)
         -> std::tuple<gsl_matrix_int*, gsl_matrix_char*>;
 
-    /// <summary>
-    /// Constructing an LCS from the length matrix
-    /// </summary>
-    /// <param name="seq1">string sequence 1</param>
-    /// <param name="direction_matrix">direction matrix</param>
-    /// <param name="length1">length of the string sequence 1</param>
-    /// <param name="length2">length of the string sequence 2</param>
-    /// <param name="result">LCS string</param>
+    /// @brief Constructing an LCS from the length matrix
+    /// @param seq1 string sequence 1
+    /// @param direction_matrix direction matrix
+    /// @param length1 length of the string sequence 1
+    /// @param length2 length of the string sequence 2
+    /// @param result LCS string
     void LongestCommonSubsequence(const std::string& seq1, const gsl_matrix_char* direction_matrix,
         int length1, int length2, std::string& result);
 
-    /// <summary>
-    /// Print table of LCS lengths
-    /// </summary>
-    /// <param name="seq1">string sequence 1</param>
-    /// <param name="seq2">string sequence 2</param>
-    /// <param name="direction_matrix">direction matrix</param>
+    /// @brief Print table of LCS lengths
+    /// @param seq1 string sequence 1
+    /// @param seq2 string sequence 2
+    /// @param direction_matrix direction matrix
     void PrintOptimalMatrix(const std::string& seq1, const std::string& seq2,
         const gsl_matrix_char* direction_matrix);
 }
--- cpp-algorithm/src/dynamic_programming/rod_cutting.h
+++ cpp-algorithm/src/dynamic_programming/rod_cutting.h
@@ -6,46 +6,36 @@
 
 namespace RodCutting
 {
-    /// <summary>
-    /// Rod cutting algorithm to call recursively.
-    /// </summary>
-    /// <param name="price">prices for rod length</param>
-    /// <param name="length">length of a rod</param>
-    /// <returns>the maximum revenue</returns>
+    /// @brief Rod cutting algorithm to call recursively.
+    /// @param price prices for rod length
+    /// @param length length of a rod
+    /// @return the maximum revenue
     auto CutRod(const std::map<int, int>& price, const int length) -> int;
 
-    /// <summary>
-    /// Rod cutting algorithm with memoization that use top-down approach.
-    /// </summary>
-    /// <param name="price">prices for rod length</param>
-    /// <param name="length">length of a rod</param>
-    /// <returns>the maximum revenue</returns>
+    /// @brief Rod cutting algorithm with memoization that use top-down approach.
+    /// @param price prices for rod length
+    /// @param length length of a rod
+    /// @return the maximum revenue
     auto MemoizedCutRod(const std::map<int, int>& price, const int length) -> int;
 
-    /// <summary>
-    /// Sub procedure for rod cutting algorithm with memoization.
+    /// @brief Sub procedure for rod cutting algorithm with memoization.
     /// This use auxiliary array for memoization.
-    /// </summary>
-    /// <param name="price">prices for rod length</param>
-    /// <param name="length">length of a rod</param>
-    /// <param name="memo">memoized results</param>
-    /// <returns>the maximum revenue</returns>
+    /// @param price prices for rod length
+    /// @param length length of a rod
+    /// @param memo memoized results
+    /// @return the maximum revenue
     auto MemoizedCutRodAux(const std::map<int, int>& price, const int length, std::vector<int>& memo) -> int;
 
-    /// <summary>
-    /// Rod cutting algorithm that use bottom-up approach.
-    /// </summary>
-    /// <param name="price">prices for rod length</param>
-    /// <param name="length">length of a rod</param>
-    /// <returns>the maximum revenue</returns>
+    /// @brief Rod cutting algorithm that use bottom-up approach.
+    /// @param price prices for rod length
+    /// @param length length of a rod
+    /// @return the maximum revenue
     auto BottomUpCutRod(const std::map<int, int>& price, const int length) -> int;
 
-    /// <summary>
-    /// Rod cutting algorithm that computes the maximum revenue and the optimal size of the first piece for given rod length.
-    /// </summary>
-    /// <param name="price">prices for rod length</param>
-    /// <param name="length">length of a rod</param>
-    /// <returns>the maximum revenue and the optimal size of the first piece</returns>
+    /// @brief Rod cutting algorithm that computes the maximum revenue and the optimal size of the first piece for given rod length.
+    /// @param price prices for rod length
+    /// @param length length of a rod
+    /// @return the maximum revenue and the optimal size of the first piece
     auto ExtendedBottomUpCutRod(const std::map<int, int>& price, const int length) -> std::tuple<int, int>;
 }
 
