a26b64f6315b8908ebd9c381796074e0ed0ce453
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-28 20:24:20 +0900

    update lombok dependency

--- java-algorithm/build.gradle.kts
+++ java-algorithm/build.gradle.kts
@@ -13,6 +13,15 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+    testCompileOnly {
+        extendsFrom(configurations.testAnnotationProcessor.get())
+    }
+}
+
 sourceSets {
     main {
         java {
@@ -38,14 +47,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.24")
     implementation("org.slf4j:slf4j-api:2.0.5")
     implementation("org.apache.logging.log4j:log4j-api:2.19.0")
     implementation("org.apache.logging.log4j:log4j-core:2.19.0")
     implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.javatuples:javatuples:1.2")
-    implementation("org.openjdk.jmh:jmh-core:1.35")
+    compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.35")
 
@@ -56,6 +64,7 @@ dependencies {
     testImplementation("org.mockito:mockito-junit-jupiter:4.10.0")
     testImplementation("org.mockito:mockito-inline:4.10.0")
     testImplementation("org.openjdk.jmh:jmh-core:1.35")
+    testCompileOnly("org.projectlombok:lombok:1.18.24")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
     testAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.35")
 }
