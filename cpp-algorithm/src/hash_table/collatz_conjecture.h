a2604c68768ebd613c8fcf1b1f8418bfb0d7a9ce
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-05-18 01:00:22 +0900

    refactor code

--- cpp-algorithm/src/hash_table/collatz_conjecture.h
+++ cpp-algorithm/src/hash_table/collatz_conjecture.h
@@ -6,15 +6,23 @@
 
 namespace CollatzConjecture
 {
-    /// @brief Generate the Collatz sequence of a number.
-    /// @param number a positive integer
-    /// @param hash_table a hash table to store the Collatz sequence of a number
-    /// @return the Collatz sequence of a number
-    auto GenerateCollatzSequence(long long number, std::unordered_map<long long, std::vector<long long>>& hash_table) -> std::vector<long long>;
+    /**
+     * @brief Generate the Collatz sequence of a number.
+     * @details The Collatz conjecture is defined as follows:
+     * Take any natural number n. If n is even, divide it by 2 to get n / 2.
+     * If n is odd, multiply it by 3 and add 1 to obtain 3n + 1.
+     * @param number a positive integer
+     * @param hash_table a hash table to store the Collatz sequence of a number
+     * @return the Collatz sequence of a number
+     */
+    auto GenerateCollatzSequence(long long number, std::unordered_map<long long, std::vector<long long>>& hash_table)
+        -> std::vector<long long>;
 
-    /// @brief Find the numbers satisfying the Collatz conjecture.
-    /// @param number a positive integer
-    /// @return a vector of numbers satisfying the Collatz conjecture
+    /**
+     * @brief Find the numbers satisfying the Collatz conjecture.
+     * @param number a positive integer
+     * @return a vector of numbers satisfying the Collatz conjecture
+     */
     auto FindNumbersSatisfyingCollatzConjecture(long long number) -> std::vector<long long>;
 }
 
--- cpp-algorithm/src/sort/intersect_two_sorted_array.h
+++ cpp-algorithm/src/sort/intersect_two_sorted_array.h
@@ -4,36 +4,36 @@
 #include <vector>
 
 /**
- * \brief Compute the intersection of two sorted arrays.
+ * @brief Compute the intersection of two sorted arrays.
  * Given two sorted arrays, compute their intersection and return the result as a vector.
  * The input arrays may have duplicate entries, but the output should be free of duplicates.
- * \details This function is a brute force solution using two for loops.
- * \param arr1 first number array 
- * \param arr2 second number array
- * \return number array of intersection
+ * @details This function is a brute force solution using two for loops.
+ * @param arr1 first number array 
+ * @param arr2 second number array
+ * @return number array of intersection
  */
-std::vector<int> IntersectTwoSortedArrayLoopJoin(const std::vector<int>& arr1, const std::vector<int>& arr2);
+auto IntersectTwoSortedArrayLoopJoin(const std::vector<int>& arr1, const std::vector<int>& arr2) -> std::vector<int>;
 
 /**
- * \brief Compute the intersection of two sorted arrays.
+ * @brief Compute the intersection of two sorted arrays.
  * Given two sorted arrays, compute their intersection and return the result as a vector.
  * The input arrays may have duplicate entries, but the output should be free of duplicates.
- * \details This function uses two for loops, but the inner loop uses _binary search_.
- * \param arr1 first number array
- * \param arr2 second number array
- * \return number array of intersection
+ * @details This function uses two for loops, but the inner loop uses _binary search_.
+ * @param arr1 first number array
+ * @param arr2 second number array
+ * @return number array of intersection
  */
-std::vector<int> IntersectTwoSortedArrayLoopJoin2(const std::vector<int>& arr1, const std::vector<int>& arr2);
+auto IntersectTwoSortedArrayLoopJoin2(const std::vector<int>& arr1, const std::vector<int>& arr2) -> std::vector<int>;
 
 /**
- * \brief Compute the intersection of two sorted arrays.
+ * @brief Compute the intersection of two sorted arrays.
  * Given two sorted arrays, compute their intersection and return the result as a vector.
  * The input arrays may have duplicate entries, but the output should be free of duplicates.
- * \details This function uses two pointers to iterate through the arrays.
- * \param arr1 first number array
- * \param arr2 second number array
- * \return number array of intersection
+ * @details This function uses two pointers to iterate through the arrays.
+ * @param arr1 first number array
+ * @param arr2 second number array
+ * @return number array of intersection
  */
-std::vector<int> IntersectTwoSortedArray(const std::vector<int>& arr1, const std::vector<int>& arr2);
+auto IntersectTwoSortedArray(const std::vector<int>& arr1, const std::vector<int>& arr2) -> std::vector<int>;
 
 #endif
